6d34cde1e38c336be25075e826faba90f850e525
marben431 <marben431@E29658.edu.linkoping.se>
2015-02-06 11:10:42 +0100

    ny commit

--- homepage.php
+++ homepage.php
@@ -12,7 +12,6 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="main.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-        <script>
     </head>
     <body>
         <div id="wrapper">
@@ -23,13 +22,13 @@
                 <div class="logo"><img src="http://placehold.it/200x50"></div>
                 <div id="sakerimitten">
                     <div class="link mitt">  
-                        <a href="#">Fittlänk</a>
+                        <a href="#">skitlänk</a> 
                         <a href="#">Nyhetsbrev</a>
                         <a href="#">Kontakt</a>
                         <a href="#" id="rUndantag">Butiker</a>
                     </div>
                     <div class="kategorier mitt">
-                        <a class="dam" href="#"><p>DAM</p></a>
+                        <a class="herr" href="#">DAM</a>
                         <a class="herr" href="#">HERR</a>
                         <a class="barn" href="#">BARN</a>
                     </div>
