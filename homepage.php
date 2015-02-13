ec243ce9e0b36c4f09da9b7ca3e14e3f0c2a3b50
marben431 <marben431@E29658.edu.linkoping.se>
2015-02-13 09:14:27 +0100

    ny commit

--- homepage.php
+++ homepage.php
@@ -7,8 +7,8 @@
         <title>TTM - Hem</title>
         <link rel="stylesheet" href="main.css">
         <link rel="stylesheet" href="reset.css">
-        
-         <!-- Bootstrap -->
+
+        <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="main.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
@@ -17,7 +17,7 @@
         <div id="wrapper">
             <header>
                 <div class="over-head">
-                    
+
                 </div>
                 <div class="logo"><img src="http://placehold.it/200x50"></div>
                 <div id="sakerimitten">
@@ -40,7 +40,47 @@
                 </div>
             </header>
             <section>
-                <img src="http://placehold.it/1080x400">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="http://placehold.it/1080x400" alt="http://placehold.it/1080x400">
+                            <div class="carousel-caption">
+                                <p>Första Sliden!<p/>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://placehold.it/1080x400" alt="http://placehold.it/1080x400">
+                            <div class="carousel-caption">
+                                <p>Andra Sliden!<p/>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://placehold.it/1080x400" alt="http://placehold.it/1080x400">
+                            <div class="carousel-caption">
+                                <p>Tredje Sliden!<p/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                
                 <div class="img-under">
                     <img class="img-under-specific" src="http://placehold.it/350x380">
                     <img class="img-under-specific" src="http://placehold.it/350x380">
