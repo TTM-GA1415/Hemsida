0a995d7c47ab418cd773c1a28e8e912eb116a471
TTM-GA1415 <fulhuve@hotmail.com>
2015-02-27 10:59:20 +0100

    sad
    asd

--- TESTFILE.php
+++ TESTFILE.php
@@ -1,30 +1,16 @@
 <?php
 
-
-
- $bool = kollaUN("dor");
-if($bool){
-    echo "upptaget";
-}else{
-    echo "ledigt";
-}
-
-function kollaUN($username) {
-    define("DB_SERVER", "localhost");
+define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_NAME", "ttm_db");
 $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
-    
-    
-    $sql = "SELECT * FROM users WHERE anv_namn=:username";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(":username", $username);
-    $stmt->execute();
-    $user = $stmt->fetchAll();
-    if (!empty($user)) {
-        return $upptaget = true;
-    } else {
-        return $upptaget = false;
-    }
-}
+
+$userlist = "";
+$sql = "SELECT * FROM users WHERE 1";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll();
+        foreach($user as $user){
+            echo $user . "<br>";
+        }
--- admin.php
+++ admin.php
@@ -1,3 +1,23 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "ttm_db");
+$dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+
+$userlist = "";
+$sql = "SELECT * FROM users WHERE anv_namn=:username AND pass=:password";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(":username", $tmp_username);
+        $stmt->bindParam(":password", $tmp_password);
+        $stmt->execute();
+        $users = $stmt->fetchAll();
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,6 +33,7 @@
                 <?php
                 include('header.html');
                 include('registrera_produkt.php');
+                
                 ?> 
             </section>
         </div>
