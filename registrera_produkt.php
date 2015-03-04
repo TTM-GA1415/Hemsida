3fc3fecf13b179d019fcabcfe7e99b6ce41d9c68
TTM-GA1415 <fulhuve@hotmail.com>
2015-03-04 13:08:38 +0100

    Produktregister
    Taggar

--- registrera_produkt.php
+++ registrera_produkt.php
@@ -4,11 +4,11 @@ $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8
 $nyProd_form = "";
 $prodList_ul = "";
 if (isset($_GET["redProdukt"])) {
-    $prodList_ul = redigeraProduktForm();
+    $prodList_ul = redigeraProduktList();
 }
 
 if (isset($_GET["redigera_produkt"])) {
-    $nyProd_form = redigeraProduktDB();
+    $nyProd_form = redigeraProduktForm();
 }
 
 if (isset($_GET["nyProdukt"])) {
@@ -18,24 +18,7 @@ if (isset($_GET["addProdukt"])) {
     skapaProduktDB();
 }
 if (isset($_GET["updateProdukt"])) {
-    $tmpNamn = filter_input(INPUT_GET, 'namn', FILTER_SANITIZE_SPECIAL_CHARS);
-    $tmpSex = filter_input(INPUT_GET, 'sex', FILTER_SANITIZE_SPECIAL_CHARS);
-    $tmpPris = filter_input(INPUT_GET, 'pris', FILTER_SANITIZE_SPECIAL_CHARS);
-    $tmpStorlek = filter_input(INPUT_GET, 'storlek', FILTER_SANITIZE_SPECIAL_CHARS);
-    $tmpBeskrivning = filter_input(INPUT_GET, 'beskrivning', FILTER_SANITIZE_SPECIAL_CHARS);
-    $tmpFarg = filter_input(INPUT_GET, 'färg', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    $sql = 'UPDATE produktregister SET namn=:prod_namn, kön=:prod_sex, pris=:prod_pris, storlek=:prod_storlek, beskrivning=:prod_beskrivning, färg=:prod_farg WHERE id="' . $_GET["id"] . '"';
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(":prod_namn", $tmpNamn);
-    $stmt->bindParam(":prod_sex", $tmpSex);
-    $stmt->bindParam(":prod_pris", $tmpPris);
-    $stmt->bindParam(":prod_storlek", $tmpStorlek);
-    $stmt->bindParam(":prod_beskrivning", $tmpBeskrivning);
-    $stmt->bindParam(":prod_farg", $tmpFarg);
-    $stmt->execute();
-    header('Location:?');
-    exit;
+    redigeraProduktDB();
 }
 
 //Funktioner för mindre kladd
@@ -58,20 +41,24 @@ function skapaProduktForm() {
     $form .= "</select>";
     $form .= "<p>Beskrivning</p><input type='text' name='beskrivning'><br>";
     $form .= "<p>Färg</p><input type='text' name='färg'><br>";
+    $form .= "<p>Taggar</p><input type='text' name='taggar'><br>";
     $form .= "<input type='submit' name='addProdukt' value='Lägg till'>";
     $form .= "</form>";
     return $form;
 }
 
 function skapaProduktDB() {
+    $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+    
     $tmpNamn = filter_input(INPUT_GET, 'namn', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmpSex = filter_input(INPUT_GET, 'sex', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmpPris = filter_input(INPUT_GET, 'pris', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmpStorlek = filter_input(INPUT_GET, 'storlek', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmpBeskrivning = filter_input(INPUT_GET, 'beskrivning', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmpFarg = filter_input(INPUT_GET, 'färg', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpTaggar = filter_input(INPUT_GET, 'taggar', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $sql = 'INSERT INTO produktregister (namn, kön, pris, storlek, beskrivning, färg) VALUES (:prod_namn, :prod_sex, :prod_pris, :prod_storlek, :prod_beskrivning, :prod_farg)';
+    $sql = 'INSERT INTO produktregister (namn, kön, pris, storlek, beskrivning, färg, taggar) VALUES (:prod_namn, :prod_sex, :prod_pris, :prod_storlek, :prod_beskrivning, :prod_farg, :prod_taggar)';
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(":prod_namn", $tmpNamn);
     $stmt->bindParam(":prod_sex", $tmpSex);
@@ -79,10 +66,11 @@ function skapaProduktDB() {
     $stmt->bindParam(":prod_storlek", $tmpStorlek);
     $stmt->bindParam(":prod_beskrivning", $tmpBeskrivning);
     $stmt->bindParam(":prod_farg", $tmpFarg);
+    $stmt->bindParam(":prod_taggar", $tmpTaggar);
     $stmt->execute();
 }
 
-function redigeraProduktForm() {
+function redigeraProduktList() {
     $form = "";
 
     $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
@@ -93,12 +81,13 @@ function redigeraProduktForm() {
     $form .= "<ul>";
     foreach ($produkter as $produkt) {
         $form .= "<ul>";
-        $form .= "<li>" . $produkt["namn"] . "</li>";
-        $form .= "<li>" . $produkt["kön"] . "</li>";
-        $form .= "<li>" . $produkt["pris"] . "</li>";
-        $form .= "<li>" . $produkt["storlek"] . "</li>";
-        $form .= "<li>" . $produkt["beskrivning"] . "</li>";
-        $form .= "<li>" . $produkt["färg"] . "</li>";
+        $form .= "<li><p>Namn</p>" . $produkt["namn"] . "</li>";
+        $form .= "<li><p>Kön</p>" . $produkt["kön"] . "</li>";
+        $form .= "<li><p>Pris</p>" . $produkt["pris"] . "</li>";
+        $form .= "<li><p>Storlek</p>" . $produkt["storlek"] . "</li>";
+        $form .= "<li><p>Beskrivning</p>" . $produkt["beskrivning"] . "</li>";
+        $form .= "<li><p>Färg</p>" . $produkt["färg"] . "</li>";
+        $form .= "<li><p>Taggar</p>" . $produkt["taggar"] . "</li>";
         $form .= "<form method='GET'>";
         $form .= "<input type='hidden' name='id' value='" . $produkt["id"] . "'>";
         $form .= "<input type='submit' name='redigera_produkt' value='Redigera'>";
@@ -109,7 +98,7 @@ function redigeraProduktForm() {
     return $form;
 }
 
-function redigeraProduktDB() {
+function redigeraProduktForm() {
     $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
     $form = "";
 
@@ -135,10 +124,36 @@ function redigeraProduktDB() {
     $form .= "</select>";
     $form .= "<p>Beskrivning</p><input type='text' name='beskrivning' value='" . $produkter[0]["beskrivning"] . "'><br>";
     $form .= "<p>Färg</p><input type='text' name='färg' value='" . $produkter[0]["färg"] . "'><br>";
+    $form .= "<p>Taggar</p><input type='text' name='taggar' value='" . $produkter[0]["taggar"] . "'><br>";
     $form .= "<input type='hidden' name='id' value='" . $_GET["id"] . "'>";
     $form .= "<input type='submit' name='updateProdukt' value='Redigera'>";
     $form .= "</form>";
     return $form;
+}
+
+function redigeraProduktDB(){
+    $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+    
+    $tmpNamn = filter_input(INPUT_GET, 'namn', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpSex = filter_input(INPUT_GET, 'sex', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpPris = filter_input(INPUT_GET, 'pris', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpStorlek = filter_input(INPUT_GET, 'storlek', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpBeskrivning = filter_input(INPUT_GET, 'beskrivning', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpFarg = filter_input(INPUT_GET, 'färg', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tmpTaggar = filter_input(INPUT_GET, 'taggar', FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    $sql = 'UPDATE produktregister SET namn=:prod_namn, kön=:prod_sex, pris=:prod_pris, storlek=:prod_storlek, beskrivning=:prod_beskrivning, färg=:prod_farg, taggar=:prod_taggar WHERE id="' . $_GET["id"] . '"';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(":prod_namn", $tmpNamn);
+    $stmt->bindParam(":prod_sex", $tmpSex);
+    $stmt->bindParam(":prod_pris", $tmpPris);
+    $stmt->bindParam(":prod_storlek", $tmpStorlek);
+    $stmt->bindParam(":prod_beskrivning", $tmpBeskrivning);
+    $stmt->bindParam(":prod_farg", $tmpFarg);
+    $stmt->bindParam(":prod_taggar", $tmpTaggar);
+    $stmt->execute();
+    header('Location:?');
+    exit;
 }
 ?>
 <!DOCTYPE html>
