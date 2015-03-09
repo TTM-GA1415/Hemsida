514081f11125910f5a7c385cb16af0ee3a725106
TTM-GA1415 <fulhuve@hotmail.com>
2015-03-09 12:32:05 +0100

    sieg baguette

---
+++ link-information.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TMNT - Information</title>
+
+        <link rel="stylesheet" href="reset.css">
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="information.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <?php
+             include('over_header.html');
+             include('header.html');
+        ?>
+        <div id="wrapper">
+            <section>
+                <div class="banner-information">
+                    <img src="http://placehold.it/1080x150">
+                </div>
+            </section>
+        <?php
+            include('footer.html');
+        ?>
+        </div>
+    </body>
+</html>
