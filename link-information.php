de5c46b31935e8d8a3b3593059a4731419c802d4
TTM-GA1415 <fulhuve@hotmail.com>
2015-03-12 11:08:21 +0100

    0312 information klar, tre länkar kvar

--- link-information.php
+++ link-information.php
@@ -28,6 +28,64 @@ and open the template in the editor.
                 <div class="banner-information">
                     <img src="http://placehold.it/1080x150">
                 </div>
+                <div class="bilder-information">
+                    <img src="http://placehold.it/300x190">
+                    <img class="imgs" src="http://placehold.it/300x190">
+                    <img class="imgs" src="http://placehold.it/300x190">
+                </div>
+                <div class="text-information">
+                    <div class="t-i-information omoss">
+                        <h4>Om Oss</h4>
+                        <div class="t-i-i-text">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Vivamus suscipit urna 
+                                sed nisl porttitor consectetur. Vestibulum 
+                                aliquet justo leo, ac lacinia felis condimentum
+                                quis. Suspendisse malesuada lorem vel ornare 
+                                facilisis. Praesent ante nunc, dapibus vel 
+                                finibus eu, maximus sit amet libero. 
+                                Nunc laoreet ipsum erat. Proin vehicula 
+                                justo id lacus dignissim vulputate. 
+                                Maecenas non pulvinar dui, ut fermentum lectus.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="t-i-information historia">
+                        <h4>Historia</h4>
+                        <div class="t-i-i-text">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Vivamus suscipit urna 
+                                sed nisl porttitor consectetur. Vestibulum 
+                                aliquet justo leo, ac lacinia felis condimentum
+                                quis. Suspendisse malesuada lorem vel ornare 
+                                facilisis. Praesent ante nunc, dapibus vel 
+                                finibus eu, maximus sit amet libero. 
+                                Nunc laoreet ipsum erat. Proin vehicula 
+                                justo id lacus dignissim vulputate. 
+                                Maecenas non pulvinar dui, ut fermentum lectus.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="t-i-information vision">
+                        <h4>Framtid och vision</h4>
+                        <div class="t-i-i-text">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Vivamus suscipit urna 
+                                sed nisl porttitor consectetur. Vestibulum 
+                                aliquet justo leo, ac lacinia felis condimentum
+                                quis. Suspendisse malesuada lorem vel ornare 
+                                facilisis. Praesent ante nunc, dapibus vel 
+                                finibus eu, maximus sit amet libero. 
+                                Nunc laoreet ipsum erat. Proin vehicula 
+                                justo id lacus dignissim vulputate. 
+                                Maecenas non pulvinar dui, ut fermentum lectus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
         <?php
             include('footer.html');
